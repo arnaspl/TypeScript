899dac63b8408455d51fd4b2f57d19856041baa3
arnaspl <60697454+arnaspl@users.noreply.github.com>
2021-11-18 15:01:07 +0200

    v1.0

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Priskiriami tipai kintamiesiems, kad nebutu galima skirtingo tipo kintamajam
+const firstName = 'Edvardas';
+const age = 18;
+const isMale = false;
+function add(a, b) {
+    return a + b;
+}
+add(2, 2);
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Priskiriami tipai kintamiesiems, kad nebutu galima skirtingo tipo kintamajam
+const firstName: string = 'Edvardas';
+const age: number = 18;
+const isMale: boolean = false;
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+add(2, 2);
